95669e093f179d7472803d4dae21da633b88584f
matonism <michaelmatonis@hotmail.com>
2023-06-06 16:43:24 -0400

    reorganized and added server that will work

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+var path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('pages/public'));
+app.use(express.static(path.join(__dirname, '../')));
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
